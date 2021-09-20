989243695f38204bd989231878490417607620ff
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-09-20 11:08:36 +0800

    None

--- Code/1. 温度转换.py
+++ Code/1. 温度转换.py
@@ -16,3 +16,10 @@ F = C * 1.8 + 32
 输入："F54.21"
 输出："C30.92"
 '''
+tempStr = input()
+if tempStr[0] in ['F']:
+    C = (eval(tempStr[1:])-32)/1.8
+    print("C{:.2f}".format(C))
+else:
+    F = eval(tempStr[1:])*1.8+32
+    print("F{:.2f}".format(F))
